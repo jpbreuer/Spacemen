fe032b886ce4aac38d5336557ff4fdd731d12630
Testudinidae <lamc@dragonturtle.dk>
2016-04-16 22:09:56 +0200

    Breaking stuff!

--- Assets/ObjectBreaker.cs
+++ Assets/ObjectBreaker.cs
@@ -1,25 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class ObjectBreaker : MonoBehaviour
 {
     float EPSILON = 0.000f;
+    bool doBreak = true;
+    bool currentSpace = false; bool oldSpace = false;
 
     struct Polygon
     {
         public List<Vector3> vertices;
     };
 
-    Vector3 Intersect( Vector3 v1, Vector3 v2, float da, float db )
+    Vector3 Intersect(Vector3 v1, Vector3 v2, float da, float db)
     {
         return v1 + (da / (da - db)) * (v2 - v1);
     }
 
-    List<Polygon> SutherlandHodgman(Vector3 normal, float D, Polygon poly)
+    void SutherlandHodgman(Vector3 normal, float D, Polygon poly, ref Polygon interSectPoly, out Polygon frontPoly, out Polygon backPoly)
     {
-        Polygon frontPoly; frontPoly.vertices = new List<Vector3>();
-        Polygon backPoly; backPoly.vertices = new List<Vector3>();
+        frontPoly.vertices = new List<Vector3>();
+        backPoly.vertices = new List<Vector3>();
         int numVerts = poly.vertices.Count;
 
         Vector3 v1 = poly.vertices[numVerts - 1];
@@ -32,13 +35,14 @@ public class ObjectBreaker : MonoBehaviour
 
             if (distV2 > EPSILON)
             {
-                if (distV1 < - EPSILON)
+                if (distV1 < -EPSILON)
                 {
                     Vector3 i = Intersect(v2, v1, distV2, distV1);
                     float di = Vector3.Dot(normal, i) - D;
 
                     frontPoly.vertices.Add(i);
                     backPoly.vertices.Add(i);
+                    interSectPoly.vertices.Add(i);
                 }
 
                 frontPoly.vertices.Add(v2);
@@ -52,9 +56,13 @@ public class ObjectBreaker : MonoBehaviour
 
                     frontPoly.vertices.Add(i);
                     backPoly.vertices.Add(i);
+                    interSectPoly.vertices.Add(i);
                 }
                 else if (distV1 < EPSILON && distV1 > -EPSILON)
+                {
                     backPoly.vertices.Add(v1);
+                    //interSectPoly.vertices.Add(v1);
+                }
 
                 backPoly.vertices.Add(v2);
             }
@@ -63,73 +71,152 @@ public class ObjectBreaker : MonoBehaviour
                 frontPoly.vertices.Add(v2);
 
                 if (distV1 < EPSILON && distV1 > -EPSILON)
+                {
                     backPoly.vertices.Add(v1);
+                    // interSectPoly.vertices.Add(v1);
+                }
             }
 
             v1 = v2;
             distV1 = distV2;
         }
-
-        List<Polygon> newPolygons = new List<Polygon>();
-        if (frontPoly.vertices.Count > 0)
-            newPolygons.Add(frontPoly);
-        //if (backPoly.vertices.Count > 0)
-          //  newPolygons.Add(backPoly);
-
-        return newPolygons;
     }
 
-    List<int> Triangulate(Polygon poly, int index)
+    void Triangulate(Polygon poly, int index, out List<int> triangles)
     {
-        List<int> indicies = new List<int>();
+        triangles = new List<int>();
 
-        for (int i = 1, j = 2; j < poly.vertices.Count; i = j , j++)
+        for (int i = 1, j = 2; j < poly.vertices.Count; i = j, j++)
         {
-            indicies.Add(0 + index);
-            indicies.Add(i + index);
-            indicies.Add(j + index);
-        }
+            Vector3 v1 = poly.vertices[i] - poly.vertices[0];
+            Vector3 v2 = poly.vertices[j] - poly.vertices[0];
+            Vector3 n = Vector3.Cross(v1, v2).normalized;
 
-        return indicies;
+            triangles.Add(0 + index);
+            triangles.Add(i + index);
+            triangles.Add(j + index);
+        }
     }
 
-// Update is called once per frame
-void Update () {
-        if (Input.GetKey(KeyCode.Space))
+    void Triangulate(Polygon poly, int index, Vector3 normal, out List<int> triangles)
+    {
+        triangles = new List<int>();
+
+        for (int i = 1, j = 2; j < poly.vertices.Count; i = j, j++)
         {
-            Mesh m = GetComponent<MeshFilter>().mesh;
+            Vector3 v1 = poly.vertices[i] - poly.vertices[0];
+            Vector3 v2 = poly.vertices[j] - poly.vertices[0];
+            Vector3 n = Vector3.Cross(v1, v2).normalized;
+            float d = Vector3.Dot(n, normal);
 
-            int[] triangles = m.triangles;
-            Vector3[] vertices = m.vertices;
-
-            Vector3 normal = Vector3.one.normalized;
-            Vector3 point = Vector3.zero;
-            float D = Vector3.Dot(point, normal);
-
-            List<Polygon> newPolygons = new List<Polygon>();
-            Polygon tmpPoly; tmpPoly.vertices = new List<Vector3>();
-            for (int i = 0; i < triangles.Length; i += 3)
+            if (d >= 0)
             {
-                tmpPoly.vertices = new List<Vector3>();
-                tmpPoly.vertices.Add(vertices[triangles[i]]);
-                tmpPoly.vertices.Add(vertices[triangles[i+1]]);
-                tmpPoly.vertices.Add(vertices[triangles[i+2]]);
-                newPolygons.AddRange(SutherlandHodgman(normal, D, tmpPoly));
+                triangles.Add(0 + index);
+                triangles.Add(i + index);
+                triangles.Add(j + index);
             }
-
-            List<int> newTriangles = new List<int>();
-            List<Vector3> newVertices = new List<Vector3>();
-            int index = 0;
-
-            foreach (Polygon p in newPolygons)
+            else
             {
-                newVertices.AddRange(p.vertices);
-                newTriangles.AddRange(Triangulate(p, index));
-                index += p.vertices.Count;
+                triangles.Add(i + index);
+                triangles.Add(0 + index);
+                triangles.Add(j + index);
             }
-
-            GetComponent<MeshFilter>().mesh.vertices = newVertices.ToArray();
-            GetComponent<MeshFilter>().mesh.triangles = newTriangles.ToArray();
         }
+    }
+
+    void Fracture(Mesh m, Vector3 point, Vector3 normal)
+    {
+        int[] triangles = m.triangles;
+        Vector3[] vertices = m.vertices;
+
+        float D = Vector3.Dot(point, normal);
+
+        List<Polygon> topPolygons = new List<Polygon>();
+        List<Polygon> bottomPolygons = new List<Polygon>();
+        Polygon intersectPoly; intersectPoly.vertices = new List<Vector3>();
+        Polygon tmpInput, tmpAbove, tmpBelow; tmpInput.vertices = new List<Vector3>();
+
+        for (int i = 0; i < triangles.Length; i += 3)
+        {
+            tmpInput.vertices = new List<Vector3>();
+            tmpInput.vertices.Add(vertices[triangles[i]]);
+            tmpInput.vertices.Add(vertices[triangles[i + 1]]);
+            tmpInput.vertices.Add(vertices[triangles[i + 2]]);
+
+            SutherlandHodgman(normal, D, tmpInput, ref intersectPoly, out tmpAbove, out tmpBelow);
+            topPolygons.Add(tmpAbove);
+            bottomPolygons.Add(tmpBelow);
+        }
+
+        List<int> newTriangles = new List<int>();
+        List<int> tmpTriangles = new List<int>();
+        List<Vector3> newNormals = new List<Vector3>();
+        List<Vector3> newVertices = new List<Vector3>();
+        int index = 0;
+
+        foreach (Polygon p in bottomPolygons)
+        {
+            newVertices.AddRange(p.vertices);
+            newNormals.AddRange(p.vertices);
+
+            Triangulate(p, index, out tmpTriangles);
+            newTriangles.AddRange(tmpTriangles);
+
+            index += p.vertices.Count;
+        }
+        newVertices.AddRange(intersectPoly.vertices);
+        newNormals.AddRange(intersectPoly.vertices);
+
+        Triangulate(intersectPoly, index, normal, out tmpTriangles);
+        newTriangles.AddRange(tmpTriangles);
+
+        m.vertices = newVertices.ToArray();
+        m.normals = newNormals.ToArray();
+        m.triangles = newTriangles.ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentSpace = Input.GetKey(KeyCode.Space);
+        if (currentSpace && currentSpace != oldSpace && doBreak)
+        {
+            //Vector3 normal = new Vector3(Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f).normalized;
+            //Vector3 point = normal * Random.value;
+            //Fracture(point, normal);
+            Mesh thisMesh = GetComponent<MeshFilter>().mesh;
+            
+            List<Vector3> fracturePoints = new List<Vector3>();
+            List<GameObject> cubes = new List<GameObject>();
+
+            fracturePoints.Add(new Vector3(Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f) * 0.1f);
+            fracturePoints.Add(new Vector3(Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f) * 0.1f);
+            fracturePoints.Add(new Vector3(Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f) * 0.1f);
+            fracturePoints.Add(new Vector3(Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f) * 0.1f);
+            fracturePoints.Add(new Vector3(Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f) * 0.1f);
+
+            for (int i = 0; i < fracturePoints.Count; i++)
+            {
+                cubes.Add(Instantiate(this.gameObject));
+                cubes[i].GetComponent<ObjectBreaker>().doBreak = false;
+                Mesh m = cubes[i].GetComponent<MeshFilter>().mesh;
+
+                for (int j = 0; j < fracturePoints.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        Vector3 point = (fracturePoints[i] + fracturePoints[j]) / 2;
+                        Vector3 normal = (fracturePoints[j] - fracturePoints[i]).normalized;        
+
+                        Fracture(m, point, normal);
+                    }
+                }
+
+                cubes[i].transform.position = cubes[i].transform.position + (fracturePoints[i] - this.transform.position).normalized * 0.005f;
+                cubes[i].GetComponent<MeshCollider>().sharedMesh = m;
+            }
+            Destroy(this.gameObject);
+        }
+        oldSpace = currentSpace;
     }
 }
