eb77a3f164a71d94eb097ba18b80816b956d0e3d
Arne 'S Jegers <arnesjegers@gmail.com>
2016-04-16 21:29:46 +0100

    no message

---
+++ Assets/SetupScripts/CreateAsteroidBelt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateAsteroidBelt : MonoBehaviour {
+	public int AsteroidAmount = 10;
+	
+	public float mu = 1;
+	public float orbitRadius;
+	public float omega;
+	public float inclination;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
