df1ecb39b07c45b12c62d41addd4b9ca243d7511
Ricard Grove <ricard.grove@gmail.com>
2016-04-17 03:42:32 +0200

    GameOver1

---
+++ Assets/Menu/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour
+{
+    // Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKeyDown("Escape"))
+        {
+
+        }
+
+
+
+	}
+}
